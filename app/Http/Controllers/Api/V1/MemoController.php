6e63ab513129504e30dd166c4eb318325e7bb378
hishihara <ishrhnn19@gmail.com>
2019-07-18 16:17:32 +0900

    Memo　作成API

--- app/Http/Controllers/Api/V1/MemoController.php
+++ app/Http/Controllers/Api/V1/MemoController.php
@@ -29,4 +29,38 @@ class MemoController extends Controller
         $memo->load(['user']);
         return new MemoResource($memo);
     }
+
+    /**
+     * @param メモを一件追加するAPI
+     * @return Request $request
+     * @throws \Illuminate\Validation\ValidationException
+     */
+
+    public function store(Request $request)
+    {
+        $this->validate($request , [
+            'title' => 'required|string',
+            'content' => 'required|string',
+            'image' => 'image',
+        ]);
+        
+        $memo = new Memo();
+
+        if ($request->hasFile('image')) {
+            $path = $request->image->store('images', 'public');
+            $memo->image_path = $path;
+        }
+
+        $memo->title = $request->input('title');
+        $memo->content = $request->input('content');
+        $memo->user_id = $request->user()->id;
+
+
+        $memo->save();
+
+
+        return(new MemoResource($memo))
+            ->response()
+            ->setStatusCode(201);
+    }
 }
--- app/Memo.php
+++ app/Memo.php
@@ -3,11 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Memo extends Model
 {
     /**
      * Increment OFF
+     * UUIDなので文字列は自動でインクリメントしない
      * @var bool
      */
     public $incrementing = false;
@@ -20,16 +22,14 @@ class Memo extends Model
      *
      * @var array
      */
-    protected $hidden = [
-        'id',
-    ];
+    protected $hidden = ['id', 'image_path',];
 
     /**
      * The accessors to append to the model's array form.
      *
      * @var array
      */
-    protected $appends = ['updated_at_for_human'];
+    protected $appends = ['updated_at_for_human', 'image_url'];
 
     /**
      * User
@@ -49,5 +49,19 @@ class Memo extends Model
     public function getUpdatedAtForHumanAttribute()
     {
         return $this->attributes['updated_at_for_human'] = $this->updated_at->diffForHumans();
+    }
+
+    /**
+     * 画像のURL
+     *
+     * @return |null
+     */
+    public function getImageUrlAttribute()
+    {
+        if (Storage::disk('public')->exists($this->image_path)) {
+        return $this->attributes['image_url'] = Storage::disk('public')->url($this->image_path);
+        }
+
+        return null;
     }
 }
